9bcb0e4719d525462469601aa41fa7f2d6b0ec67
KarolinaKorol19 <karolinakorol421@gmail.com>
2025-10-14 18:06:23 +0300

    Створила нову зміну

---
+++ index.js
@@ -0,0 +1,116 @@
+// Завдання 1
+
+const numbers = [1, 5, 8, 12, 3, 15, 7, 20];
+const words = ["кіт", "собака", "миша", "папуга", "хомяк"];
+
+// Функція countItems тут
+function countItems(array, condition) {
+  let count = 0;
+  for (const item of array) {
+    if (condition(item)) {
+    }
+    count++;
+  }
+}
+
+// Умови для перевірки:
+const isEven = (num) => num % 2 === 0;
+const isLarge = (num) => num > 10;
+const isShort = (word) => word.length <= 3;
+
+// Тестування
+console.log("Парних чисел:", countItems(numbers, isEven));
+console.log("Чисел більше 10:", countItems(numbers, isLarge));
+console.log("Коротких слів:", countItems(words, isShort));
+
+// Завдання 2 Калькулятор з операціями
+
+// Функція calculate тут
+const calculate = (a, b, operation) => {
+  return operation(a, b);
+};
+
+// Стрілкові функції для операцій тут
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => {
+  if (b === 0) {
+    return "Помилка при діленнні на нуль!";
+  }
+  return a / b;
+};
+
+// Тестування
+console.log(calculate(10, 5, add)); // Повинно показати 15
+console.log(calculate(10, 5, subtract)); // Повинно показати 5
+console.log(calculate(10, 5, multiply)); // Повинно показати 50
+console.log(calculate(10, 5, divide)); // Повинно показати 2
+console.log(calculate(10, 0, divide)); // Повинно показати помилку
+
+// Завдання 3 Генератор повідомлень
+function repeatMessage(times, messageCreator) {
+  for (let i = 0; i < times; i++) {
+    messageCreator(i);
+  }
+}
+
+const simpleMessage = (i) => console.log(`Повідомлення №${i + 1}`);
+const greetMessage = (i) => console.log(`👋 Привіт! Це привітання №${i + 1}`);
+const countdownMessage = (i) => console.log(`Зворотний відлік: ${3 - i}`);
+
+// --- Тестування ---
+console.log("--- Просте повідомлення ---");
+repeatMessage(3, simpleMessage);
+console.log("--- Вітання ---");
+repeatMessage(5, greetMessage);
+console.log("--- Зворотний відлік ---");
+repeatMessage(3, countdownMessage);
+
+// Завдання 4 Кінотеатр (творче та додаткове завдання)
+const movies = [
+  "Avatar",
+  "Harry Potter",
+  "Pirates of the Caribbean",
+  "Carolina",
+  "Kingdom of heaven",
+];
+
+// Основна функція
+function processMovies(movies, action) {
+  for (let i = 0; i < movies.length; i++) {
+    action(movies[i], i);
+  }
+}
+
+// --- Колбек-функції ---
+const showMovie = (movie, index) => {
+  console.log(`${index + 1}. ${movie}`);
+};
+
+const addEmoji = (movie, index) => {
+  console.log(` ${movie}`);
+};
+
+const watchMovie = (movie, index) => {
+  console.log(` Я дивлюсь "${movie}" (${index + 1} з ${movies.length})`);
+};
+
+const countLetters = (movie) => {
+  console.log(`"${movie}" має ${movie.length} літер`);
+};
+
+// --- Тестування ---
+console.log("--- Список фільмів ---");
+processMovies(movies, showMovie);
+
+console.log("\n--- З емоджі ---");
+processMovies(movies, addEmoji);
+
+console.log("\n--- Перегляд фільмів ---");
+processMovies(movies, watchMovie);
+
+console.log("\n--- Кількість літер ---");
+processMovies(movies, countLetters);
+
+// the end
